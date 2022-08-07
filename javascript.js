c845c47ceecdd051f13eaa9dcab010c3a95232cc
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2022-08-07 20:43:52 +0200

    Add drawing functionality

--- javascript.js
+++ javascript.js
@@ -5,10 +5,25 @@ for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
         let cell = document.createElement('div')
         cell.className = 'cell'
+        let isMouseDown = false;
+        document.addEventListener('mousedown', function (event) {
+            isMouseDown = true;
+        });
+        document.addEventListener('mouseup', function (event) {
+            isMouseDown = false;
+        }); 
+        cell.addEventListener('mousemove', function (event) {
+            if (isMouseDown) {
+                cell.style.backgroundColor = 'black'
+            }
+        });
+        cell.addEventListener('mousedown', function (event) {
+            cell.style.backgroundColor = 'black'
+        });
         row.appendChild(cell)
     }
     document.getElementById('grid').appendChild(row);
 }
 }
 
-makeGrid(16)
+makeGrid(10)
